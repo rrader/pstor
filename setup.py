8cdceda53b6f7ca958276af98e5f40f5b602c8cd
Roman Rader <antigluk@gmail.com>
2013-02-01 14:30:03 +0200

    Initial

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+ 
+setup(name = "pstor",
+      version = "0.01",
+      description = "Encrypted storage manager",
+      author = "Roman Rader",
+      author_email = "antigluk@gmail.com",
+      packages=["pstor"]
+      )
