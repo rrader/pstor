42ee41c4c2c414e0e24c35d7a3e1316c7854bdae
Roman Rader <antigluk@gmail.com>
2013-02-03 17:11:50 +0200

    status

--- pstor/commands/status.py
+++ pstor/commands/status.py
@@ -1,10 +1,14 @@
 import os
+import sh
 from ..helpers import exceptions
 from . import cli_command
 
 @cli_command
 def status(**args):
     if os.path.isdir(".pstor"):
-        pass
+        cwd = os.path.join(os.getcwd(), 'files')
+        if any(cwd == path.strip() for path in 
+            list(sh.awk(sh.grep(sh.mount(), 'encfs'), "{print $3}"))):
+            print "All ok"
     else:
         raise exceptions.PstorException("Not initialized in this directory. Do\n $ pstor init")
