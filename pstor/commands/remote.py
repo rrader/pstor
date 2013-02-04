520d1a96687f5621a330c26007de895891d1adcf
Roman Rader <antigluk@gmail.com>
2013-02-04 12:27:30 +0200

    removing remote

--- pstor/commands/remote.py
+++ pstor/commands/remote.py
@@ -1,6 +1,7 @@
 import os
 import ConfigParser
 
+from ..helpers import exceptions
 from . import cli_command, inside_pstor
 from .providers import get_provider_for_url, get_provider_by_name
 
@@ -8,6 +9,7 @@ def args(subparsers):
     remote = subparsers.add_parser('remote')
     remote.add_argument('--list', action="store_true", default=False)
     remote.add_argument('--add', nargs=2)
+    remote.add_argument('--remove')
 
 @cli_command(args=args)
 @inside_pstor
@@ -16,8 +18,11 @@ def remote(**args):
         os.listdir('.pstor/remotes')
     if args['add']:
         name, url = args['add']
+
+        if os.path.exists(os.path.join('.pstor/remotes', name)):
+            raise exceptions.PstorException("Remote with this name already exists")
+
         provider = get_provider_for_url(url)
-        print "{}: {} => {}".format(name, url, provider.name())
         config = ConfigParser.ConfigParser()
         config.add_section("remote")
         config.set("remote", "name", name)
@@ -26,3 +31,11 @@ def remote(**args):
 
         with file(".pstor/remotes/{}".format(name), "wb") as f:
             config.write(f)
+        
+        print "{} remote '{}' for {} added".format(provider.name(), name, url)
+
+    if args['remove']:
+        name = args['remove']
+        if not os.path.exists(os.path.join('.pstor/remotes', name)):
+            raise exceptions.PstorException("Remote with this name not exists")
+        os.remove(os.path.join('.pstor/remotes', name))
